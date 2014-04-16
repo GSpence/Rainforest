e6355bddafd12a3758ef9b03a9f057974cfce204
GSpence <spencegarett@gmail.com>
2014-04-16 16:43:09 -0400

    Creating log in functionality

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rainforest::Application.routes.draw do
   get "users/create"
   resources :products
   resources :users, :only => [:new, :create]
+  resources :sessions, :only => [:new, :create, :destroy]
 end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
